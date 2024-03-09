27ed58c6542368ca8c3da633e0639d345ec2b23e
PrateekTechmatys <prateeky562@gmail.com>
2024-03-09 10:30:12 +0530

    doing the assignment

--- vite-project/src/Table.jsx
+++ vite-project/src/Table.jsx
@@ -1,46 +1,108 @@
-import React, { useEffect ,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 const Table = () => {
-    const [data, setData] =useState([])
-    const handleEdit =()=>{
-        console.log('edit')
-    }
-    useEffect(()=>{
+    const [data, setData] = useState([]);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [editId, setEditId] = useState(-1);
+    const [updateName, setUpdateName] = useState('');
+    const [updateEmail, setUpdateEmail] = useState('');
+
+    useEffect(() => {
         axios.get('http://localhost:3000/user')
-        .then((response) => 
-        setData(response.data))
-        .catch((error) =>console.log(error));
-    })
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-            {
-                data.map((user, index)=>{
-                    return(
-                        <tr key={index}>
-                            <td>{user.id}</td>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
-                            <td>
-                                <button onClick={handleEdit}>Edit</button>
-                                <button>Delete</button>
-                            </td>
-                        </tr>
-                    )
-                })
-            }
-        </tbody>
-      </table>
-    </div>
-  )
+            .then((response) =>
+                setData(response.data))
+            .catch((error) => console.log(error));
+    }, []);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const id = data.length + 1;
+        axios.post('http://localhost:3000/user', {
+            id: id,
+            name: name,
+            email: email
+        })
+            .then((response) => location.reload())
+            .catch((error) => console.log(error));
+    }
+
+    const handleUpdate = () => {
+        axios.put('http://localhost:3000/user/' + editId, {
+            id: editId,
+            name: updateName,
+            email: updateEmail
+        }).then((response) => {
+            location.reload();
+            setEditId(-1);
+        }).catch(error => console.log(error));
+
+    }
+
+    const handleEdit = (Idchange) => {
+        axios.get('http://localhost:3000/user/' + Idchange)
+            .then((response) => {
+                console.log(response);
+                // setUpdateName(response.data.name)
+                // setUpdateEmail(response.data.email)
+            })
+            .catch((error) => console.error(error));
+        setEditId(Idchange);
+    }
+
+    const handleDelete = (deleteId) => {
+        axios.delete('http://localhost:3000/user/' + deleteId)
+            .then((response) => location.reload())
+            .catch((error) => console.log(error));
+    }
+    return (
+        <div>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input type='text' placeholder='enter your name' onChange={(e) => {
+                        setName(e.target.value);
+                    }}></input>
+                    <input type='text' placeholder='Enter your email address' onChange={(e) => {
+                        setEmail(e.target.value);
+                    }}></input>
+                    <button type='submit'> ADD</button>
+                </form>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((user, index) => (
+                            user.id === editId ?
+                                <tr>
+                                    <td>{user.id}</td>
+                                    <td><input type='text' value={updateName} onChange={(e) => setUpdateName(e.target.value)}></input></td>
+                                    <td><input type='text' value={updateEmail} onChange={(e) => setUpdateEmail(e.target.value)}></input></td>
+                                    <td><button onClick={handleUpdate}>Update</button></td>
+
+                                </tr> :
+                                <tr key={index}>
+                                    <td>{user.id}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>
+                                        <button onClick={() => handleEdit(user.id)}>Edit</button>
+                                        <button onClick={() => handleDelete(user.id)}>Delete</button>
+                                    </td>
+                                </tr>
+
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Table
